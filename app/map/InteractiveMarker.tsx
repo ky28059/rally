bbcd7c1e323071be324292222d426cdb17d03cf2
tillvit <lucas.j.tan@gmail.com>
2023-09-17 12:10:10 -0400

    Allow multiple events in same building show up on map

--- app/map/InteractiveMarker.tsx
+++ app/map/InteractiveMarker.tsx
@@ -1,26 +1,33 @@
-import {useState} from 'react';
-import type {Event as EventProps} from '@/util/events'
-import {InfoWindow, Marker} from '@react-google-maps/api';
-import Event from '@/components/Event';
+import Event from "@/components/Event"
+import type { Building, Event as EventProps } from "@/util/events"
+import { InfoWindow, Marker } from "@react-google-maps/api"
 
 type InteractiveMarkerProps = {
-    event: EventProps,
-    position: {lat: number, lng: number}
-    openId: string,
-    setOpenId: (openId: string) => void
+	building: Building
+	events: EventProps[]
+	openBuilding: string
+	setOpenBuilding: (openId: string) => void
 }
 
 export default function InteractiveMarker(props: InteractiveMarkerProps) {
-    return (<>
-            <Marker
-                key={props.event.id}
-                position={props.position}
-                title={props.event.title}
-                onClick={() => props.setOpenId(props.event.id)}
-            >
-                {props.openId == props.event.id && <InfoWindow onCloseClick={() => props.setOpenId("")}>
-                    <Event {...props.event} ></Event>
-                </InfoWindow>}
-            </Marker>
-        </>)
+	return (
+		<Marker
+			key={props.building.abbr}
+			position={{ lat: props.building.lat, lng: props.building.lng }}
+			label={props.events.length > 1 ? `${props.events.length}` : undefined}
+			onClick={() => props.setOpenBuilding(props.building.abbr)}
+        >
+			{props.openBuilding == props.building.abbr && (
+				<InfoWindow
+					position={{ lat: props.building.lat, lng: props.building.lng }}
+					onCloseClick={() => props.setOpenBuilding("")}>
+					<div className="flex gap-2 overflow-x-auto overflow-y-clip w-max">
+						{props.events.map(event => (
+							<Event key={event.id} className="w-72" {...event}></Event>
+						))}
+					</div>
+				</InfoWindow>
+			)}
+		</Marker>
+	)
 }
--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,68 +1,79 @@
-'use client'
+"use client"
 
-import { GoogleMap, useLoadScript, Marker, useJsApiLoader } from '@react-google-maps/api';
-import {collection, doc} from 'firebase/firestore';
-import {useFirestore, useFirestoreCollectionData, useFirestoreDocData} from 'reactfire';
-import InteractiveMarker from '@/app/map/InteractiveMarker';
-import type {Event} from '@/util/events'
-import {useState} from 'react';
+import InteractiveMarker from "@/app/map/InteractiveMarker"
+import type { Building, Event } from "@/util/events"
+import { GoogleMap, useLoadScript } from "@react-google-maps/api"
+import { collection } from "firebase/firestore"
+import { useState } from "react"
+import { useFirestore, useFirestoreCollectionData } from "reactfire"
 
+export default function InteractiveMap() {
+	const firestore = useFirestore()
+	const { data: events } = useFirestoreCollectionData(collection(firestore, "events"))
+	const { data: buildings } = useFirestoreCollectionData(collection(firestore, "buildings"))
 
-export default function Map(){
-    const firestore = useFirestore();
-    const {data: events} = useFirestoreCollectionData(collection(firestore, 'events'));
-    const {data: buildings} = useFirestoreCollectionData(collection(firestore, 'buildings'));
+	const [openBuilding, setOpenBuilding] = useState("")
 
-    const [openId, setOpenId] = useState("")
+	const options = {
+		styles: [
+			{
+				featureType: "all",
+				elementType: "labels",
+				stylers: [{ visibility: "off" }],
+			},
+		],
+	}
 
-    const options = {
-        styles: [
-            {
-                featureType: 'all',
-                elementType: 'labels',
-                stylers: [{ visibility: 'off' }],
-            },
-        ],
-    };
+	const mapContainerStyle = {
+		height: "100vh",
+		width: "100%",
+	}
 
-    const mapContainerStyle = {
-        height: '100vh',
-        width: '100%',
-    };
+	const center = {
+		lat: 40.4237,
+		lng: -86.9212,
+	}
 
-    const center = {
-        lat: 40.4237,
-        lng: -86.9212,
-    };
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+	})
 
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
-    });
+	function groupEvents(events: Event[]) {
+		const locs: Map<Building, Event[]> = new Map()
+		events.forEach(event => {
+			if (!event.location) return
+			const locationFirst = event.location.split(" ")[0].toLowerCase()
+			const buildingMatch = buildings?.find(
+				building =>
+					building.name.toLowerCase() === locationFirst ||
+					building.abbr.toLowerCase() === locationFirst
+			) as Building | undefined
+			if (buildingMatch) {
+				if (!locs.has(buildingMatch)) locs.set(buildingMatch, [])
+				locs.get(buildingMatch)?.push(event)
+			}
+		})
+		return locs
+	}
 
-    if (!isLoaded) return <div> Loading....</div>;
-    return (
-        <GoogleMap
-            zoom={15}
-            mapContainerStyle={mapContainerStyle}
-            center={center}
-            options={options}
-            onClick={() => setOpenId("")}
+	if (!isLoaded) return <div> Loading....</div>
+	return (
+		<GoogleMap
+			zoom={15}
+			mapContainerStyle={mapContainerStyle}
+			center={center}
+			options={options}
+			onClick={() => setOpenBuilding("")}    
         >
-            {events && events.map((event, index) => {
-                if (!event.location) return
-                // Check if the location matches a building/building abbreviation
-                const locationFirst = event.location.split(" ")[0].toLowerCase()
-                const buildingMatch = buildings?.find(building => building.name.toLowerCase() === locationFirst || building.abbr.toLowerCase() === locationFirst)
-                if (buildingMatch) return (
-                    <InteractiveMarker
-                        position={{ lat: buildingMatch.lat, lng: buildingMatch.lng }}
-                        event={event as Event}
-                        openId={openId}
-                        setOpenId={setOpenId}
-                    />
-                )
-                return
-            })}
-        </GoogleMap>
-    );
+			{events && [...groupEvents(events as Event[]).entries()].map(([loc, events]) => (
+                <InteractiveMarker
+                    key={loc.abbr}
+                    building={loc}
+                    events={events}
+                    openBuilding={openBuilding}
+                    setOpenBuilding={setOpenBuilding}
+                />
+            ))}
+		</GoogleMap>
+	)
 }
