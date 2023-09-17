4f5dbd03ecf2dde9d666324fe3593c26c07c4a49
Kevin Yu <kevinyu567@gmail.com>
2023-09-16 23:13:35 -0400

    Actually fix build for real this time on god

--- util/firebase.ts
+++ util/firebase.ts
@@ -6,7 +6,7 @@ admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.PROJECT_ID,
         clientEmail: process.env.CLIENT_EMAIL,
-        privateKey: process.env.PRIVATE_KEY
+        privateKey: process.env.PRIVATE_KEY.replaceAll('\\n', '\n')
     })
 })
 
